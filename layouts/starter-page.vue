7529dcab674f4f784f3eb72ec741a66ad9b6929c
João Wesley <joaowesley527@gmail.com>
2020-07-22 00:23:27 -0300

    adding background for starting page

--- layouts/starter-page.vue
+++ layouts/starter-page.vue
@@ -1,15 +1,36 @@
 <template>
-  <v-app id="starter-page">
+  <v-app id="starter-page" class="bg-img">
     <v-content>
       <v-container>
         <nuxt />
       </v-container>
     </v-content>
+    <v-footer dark padless>
+      <v-card class="flex" flat tile>
+        <v-card-text dark class="py-2 white--text text-center">
+          {{ new Date().getFullYear() }} —
+          <strong>English everywhere</strong>
+        </v-card-text>
+      </v-card>
+    </v-footer>
   </v-app>
 </template>
 
 <script>
-export default {}
+export default {
+  data: () => ({
+    drawer: false,
+  }),
+}
 </script>
 
-<style scoped></style>
+
+<style scoped>
+.bg-img {
+  background-image: url('~@/assets/index.jpg');
+  background-position: center center;
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-size: cover;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,4 @@
 <template>
-  <!-- <v-layout column justify-center align-center /> -->
   <v-container class="fill-height" fluid>
     <LoginRegisterReset v-if="!token || showLoginForm" />
     <ResetPassword
@@ -8,6 +7,9 @@
       :user-id="userId"
       @showLoginForm="showLoginForm = true"
     />
+    <div class="bg-text">
+      <h1>Aprenda inglês com conteúdo do seu interesse</h1>
+    </div>
   </v-container>
 </template>
 
@@ -35,3 +37,21 @@ export default {
   layout: 'starter-page',
 }
 </script>
+
+<style>
+.bg-text {
+  background-color: rgb(0, 0, 0); /* Fallback color */
+  background-color: rgba(0, 0, 0, 0.4); /* Black w/opacity/see-through */
+  color: white;
+  font-weight: bold;
+  border: 3px solid #f1f1f1;
+  position: absolute;
+  top: 70%;
+  left: 50%;
+  margin: 0 auto;
+  transform: translate(-50%, -50%);
+  width: 80%;
+  padding: 20px;
+  text-align: center;
+}
+</style>
